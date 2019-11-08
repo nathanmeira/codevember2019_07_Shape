59a1e05619691385d8a9e982b645cc6b4ce37f6c
phcacique <phcacique@gmail.com>
2019-11-08 00:59:11 -0300

    First commit

--- Shape/GameScene.swift
+++ Shape/GameScene.swift
@@ -11,76 +11,105 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    let images:[String] = ["monalisa_50x75","moca_50x72","beijo_50x69","grito_50x64"]
+    var currentImage:Int = 0
+    var colors:[[UIColor]] = []
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
+        let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
+
+        leftSwipe.direction = .left
+        rightSwipe.direction = .right
+
+        view.addGestureRecognizer(leftSwipe)
+        view.addGestureRecognizer(rightSwipe)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        restart()
+    }
+    
+    func restart(){
+        removeAllChildren()
+        removeAllActions()
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        colors = []
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        if let image = UIImage(named: images[currentImage]){
+            let rgba = RGBA(image)
+            for x in 0..<rgba.width {
+                var temp:[UIColor] = []
+                for y in 0..<rgba.height {
+                    let index = y * rgba.width + x
+                    let pixel = rgba.pixels[index]
+                    temp.append(UIColor(red: CGFloat(pixel.red)/CGFloat(255), green: CGFloat(pixel.green)/CGFloat(255), blue: CGFloat(pixel.blue)/CGFloat(255), alpha: 1))
+                }
+                colors.append(temp)
+            }
+            showImage()
+        }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    func showImage(){
+        
+        let s = CGFloat(UIScreen.main.bounds.size.width) / CGFloat(colors.count)
+        let border:CGFloat = UIScreen.main.bounds.size.height - (s * CGFloat(colors[0].count))
+        
+        for x in 0..<colors.count {
+            for y in 0..<colors[x].count {
+                drawArc(center: CGPoint(x:CGFloat(x) * s - UIScreen.main.bounds.size.width/2 + s / 2,
+                                        y: CGFloat(y) * s - UIScreen.main.bounds.size.height/2 + s / 2 + border / 2) ,
+                        radius: CGFloat.random(in:1...3) * s,
+                        height: s/4,
+                        color:colors[x][colors[x].count - 1 - y])
+            }
+        }
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    func drawArc(center:CGPoint, radius:CGFloat = 100, startAngle:Int = 0, endAngle:Int = 360, height:CGFloat = 20, color:UIColor = .yellow, randomAlpha:Bool = true) {
+
+        let startRad:CGFloat = CGFloat(startAngle) * .pi / 180
+        let endRad:CGFloat = CGFloat(endAngle) * .pi / 180
+        
+        let path = UIBezierPath()
+        path.addArc(withCenter: center,
+                  radius: radius,
+                  startAngle: startRad,
+                  endAngle: endRad,
+                  clockwise: true)
+
+        let section = SKShapeNode(path: path.cgPath)
+        section.position = CGPoint(x: size.width/2, y: size.height/2)
+        section.lineWidth = height
+        section.strokeColor = color
+        if randomAlpha {
+            section.alpha = CGFloat.random(in: 0.3...1)
+        } else {
+            section.alpha = 0.2
+        }
+        addChild(section)
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
+    @objc func handleSwipes(_ sender:UISwipeGestureRecognizer) {
+               
+           if (sender.direction == .left) {
+               currentImage -= 1
+               if currentImage < 0{
+                   currentImage = images.count-1
+               }
+               restart()
+            print("left")
+           }
+               
+           if (sender.direction == .right) {
+               currentImage += 1
+               if currentImage >= images.count{
+                   currentImage = 0
+               }
+               restart()
+            print("right")
+           }
+       }
     
     
     override func update(_ currentTime: TimeInterval) {
